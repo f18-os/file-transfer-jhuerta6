0e54f97cc16f4d9e227945a8b2315076d673ffde
jhuerta6 <jorgehuertahu@gmail.com>
2018-10-04 07:40:10 -0600

    reads a file each 100 bytes, sends it, receives it encoded with 'b'

---
+++ file-transfer-lab/fileClient.py
@@ -0,0 +1,81 @@
+#! /usr/bin/env python3
+
+# Echo client program
+import socket, sys, re
+
+sys.path.append("../lib")       # for params
+import params
+
+from framedSock import framedSend, framedReceive
+
+
+switchesVarDefaults = (
+    (('-s', '--server'), 'server', "127.0.0.1:50001"),
+    (('-d', '--debug'), "debug", False), # boolean (set if present)
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+
+progname = "framedClient"
+paramMap = params.parseParams(switchesVarDefaults)
+
+server, usage, debug  = paramMap["server"], paramMap["usage"], paramMap["debug"]
+
+if usage:
+    params.usage()
+
+
+try:
+    serverHost, serverPort = re.split(":", server)
+    serverPort = int(serverPort)
+except:
+    print("Can't parse server:port from '%s'" % server)
+    sys.exit(1)
+
+s = None
+for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
+    af, socktype, proto, canonname, sa = res
+    try:
+        print("creating sock: af=%d, type=%d, proto=%d" % (af, socktype, proto))
+        s = socket.socket(af, socktype, proto)
+    except socket.error as msg:
+        print(" error: %s" % msg)
+        s = None
+        continue
+    try:
+        print(" attempting to connect to %s" % repr(sa))
+        s.connect(sa)
+    except socket.error as msg:
+        print(" error: %s" % msg)
+        s.close()
+        s = None
+        continue
+    break
+
+if s is None:
+    print('could not open socket')
+    sys.exit(1)
+
+
+# print("sending hello world")
+# framedSend(s, b"hello world", debug)
+# print("received:", framedReceive(s, debug))
+#
+# print("sending hello world")
+# framedSend(s, b"hello world", debug)
+# print("received:", framedReceive(s, debug))
+#
+# while 1:
+#     user_msg = input().encode()
+#     if user_msg.decode() == "exit": exit()
+#     framedSend(s, user_msg, debug)
+#     print("received:", framedReceive(s, debug))
+
+file = open("smpl.txt", "rb")
+send = file.read(100)
+while 1:
+    framedSend(s, send, debug)
+    send = file.read(100)
+    print("received:", framedReceive(s, debug))
+file.close()
+s.close()
